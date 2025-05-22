e0d6d3175fa6b576094f24869b8b7cdccd029e13
Helio Ferraz <amorimsantosferrazhelio@gmail.com>
2025-05-22 15:23:14 -0300

    Add files via upload

---
+++ Testewhile.py
@@ -0,0 +1,12 @@
+
+
+
+
+Numero = int(input("Qual numero você vai começar?"))
+multiplicador = int(input("Por qual numero você vai multiplicar?"))
+final = int(input("Até qual numero você vai multiplicar?"))
+print("\n--------------------------------------------")
+
+while Numero <= final:
+    print(f" {Numero} x {multiplicador} = {multiplicador * Numero}")
+    Numero += 1
